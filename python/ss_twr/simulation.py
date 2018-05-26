60ace40a803eb0bb6bfc5a6dbf448e049e9dbf97
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-05-26 12:42:12 +0200

    Use pandas in analysis module

--- python/ss_twr/simulation.py
+++ python/ss_twr/simulation.py
@@ -21,10 +21,9 @@ import numpy as np
 import smile.area as sarea
 import smile.simulation as ssimulation
 from smile.filter import Filter
-from smile.frames import Frames
+from smile.array import Array
 from smile.nodes import Nodes
 from smile.results import Results
-from ss_twr.anchors import Anchors
 import scipy.constants as scc
 
 
@@ -44,12 +43,16 @@ class Simulation(ssimulation.Simulation):
         self.c *= 1e-12  # m/s -> m/ps
 
     def run_offline(self, directory_path):
-        anchors = Anchors.load_csv(os.path.join(directory_path, 'ss_twr_anchors.csv'))
-        mobiles = Nodes.load_csv(os.path.join(directory_path, 'ss_twr_mobiles.csv'))
-        mobile_frames = Frames.load_csv(os.path.join(directory_path, 'ss_twr_mobile_frames.csv'))
+        mobiles = Array.load_csv(Array.prepare_nodes_type(), os.path.join(directory_path, 'ss_twr_mobiles.csv'))
 
-        results = None
-        for mobile_node in mobiles:
+        mobile_frames = Array.load_csv(Array.prepare_frames_type(),
+                                       os.path.join(directory_path, 'ss_twr_mobile_frames.csv'))
+
+        anchors = Array.load_csv(Array.prepare_nodes_type([('message_processing_time', np.int64)]),
+                                 os.path.join(directory_path, 'ss_twr_anchors.csv'))
+
+        results = Array.create(Array.prepare_results_type())
+        for _, mobile_node in mobiles.iterrows():
             mobile_results = self._localize_mobile(mobile_node, anchors, mobile_frames)
             if results is None:
                 results = mobile_results
@@ -58,45 +61,58 @@ class Simulation(ssimulation.Simulation):
 
         return results, anchors
 
-    def _localize_mobile(self, mobile_node, anchors, mobile_frames):
+    def _localize_mobile(self, mobile_node, all_anchors, all_mobile_frames):
         # Construct POLL frames filter, i.e. transmitted frames ('TX' directions) sent by mobile node
-        data_filter = Filter()
-        data_filter.equal("direction", hash('TX'))
-        data_filter.equal("source_mac_address", mobile_node["mac_address"])
-        poll_frames = data_filter.execute(mobile_frames)
+        #data_filter = Filter()
+        #data_filter.equal("direction", hash('TX'))
+        #data_filter.equal("source_mac_address", mobile_node["mac_address"])
+        #poll_frames = data_filter.execute(mobile_frames)
+        poll_frames = all_mobile_frames.query(f'source_mac_address == {mobile_node["mac_address"]} and direction == \'TX\'')
 
         # Construct REPONSE frames filter, i.e. transmitted frames ('RX' directions) sent to mobile node
-        data_filter = Filter()
-        data_filter.equal("direction", hash('RX'))
-        data_filter.equal("destination_mac_address", mobile_node["mac_address"])
-        response_frames = data_filter.execute(mobile_frames)
+        #data_filter = Filter()
+        #data_filter.equal("direction", hash('RX'))
+        #data_filter.equal("destination_mac_address", mobile_node["mac_address"])
+        response_frames = all_mobile_frames.query(f'destination_mac_address == {mobile_node["mac_address"]} and direction == \'RX\'')
 
-        assert (np.unique(anchors["message_processing_time"]).shape == (1,))
-        processing_delay = anchors[0, "message_processing_time"]
+        #assert (np.unique(anchors["message_processing_time"]).shape == (1,))
+        #processing_delays = all_anchors["message_processing_time"]
 
         sequence_numbers_triples = self._lookup_sequence_number_triples(poll_frames["sequence_number"],
                                                                         response_frames["sequence_number"])
-        results = Results.create_array(1, mac_address=mobile_node["mac_address"])
+        results = [] #Results.create_array(1, mac_address=mobile_node["mac_address"])
 
         for round_i in range(len(sequence_numbers_triples)):
             sequence_numbers = sequence_numbers_triples[round_i]
 
-            frames_filter = Filter()
-            frames_filter.is_in("sequence_number", sequence_numbers)
-            round_poll_frames = frames_filter.execute(poll_frames)
-            round_response_frames = frames_filter.execute(response_frames)
+            #frames_filter = Filter()
+            #frames_filter.is_in("sequence_number", sequence_numbers)
+            #round_poll_frames = frames_filter.execute(poll_frames)
+            #round_response_frames = frames_filter.execute(response_frames)
+            round_poll_frames = poll_frames.query(f'sequence_number in {sequence_numbers}')
+            round_response_frames = response_frames.query(f'sequence_number in {sequence_numbers}')
+            anchors = all_anchors.query(f'mac_address in {list(round_response_frames["source_mac_address"])}')
 
-            tof = round_response_frames["begin_clock_timestamp"] - round_poll_frames["begin_clock_timestamp"]
-            tof -= processing_delay
-            tof /= 2
+            tof = round_response_frames["begin_clock_timestamp"].values
+            tof -= round_poll_frames["begin_clock_timestamp"].values
+            tof -= anchors["message_processing_time"].values
+            tof = tof / 2
 
-            distances = np.zeros((3,))
-            distances[:] = tof * self.c
+            distances = tof * self.c
 
-            solver = self.Solver(anchors[0:3, "position_2d"], distances, self.solver_configuration)
-            position = solver.localize()
+            anchors_coordinates = anchors[['position_x', 'position_y']].values
+            solver = self.Solver(anchors_coordinates, distances, self.solver_configuration)
+            positions = solver.localize()
+            position = positions[0]
 
-            results[round_i, "position_2d"] = position[0:2]
+            result = Results()
+            result.mac_address = mobile_node["mac_address"]
+            result.position_dimensions = 2
+            result.position_x = position[0]
+            result.position_y = position[1]
+            result.position_z = 0
+
+            results[round_i, "position_2d"] = position[0]
             results[round_i, "begin_true_position_2d"] = round_poll_frames[0, "begin_true_position_2d"]
             results[round_i, "end_true_position_2d"] = round_response_frames[2, "end_true_position_2d"]
 
